a9811b94ad7107f5745b6e7d91788a6c12d85c11
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-05-24 17:59:40 +0800

    End of program

--- test_tv.py
+++ test_tv.py
@@ -21,7 +21,7 @@ def test_tv():
 # Increase the volume of tv 1 by 1
     # tv_1.volume_up()
 # Decrease the volume of tv 1 by 1
-    tv_1.volume_down()
+    # tv_1.volume_down()
 # Set channel for tv 2 
     tv_2.set_channel(3)
 # Increase channel number of tv 2 by 1
@@ -33,9 +33,11 @@ def test_tv():
 # Increase the volume of tv 2 by 1 
     # tv_2.volume_up()
 # Decrease the volume of tv 2 by 1
-    tv_2.volume_down()
+    # tv_2.volume_down()
 # Print the output
     print("tv1's channel is", tv_1.get_channel(), "and the volume level is", tv_1.get_volume())
     print("tv2's channel is", tv_2.get_channel(), "and the volume level is", tv_2.get_volume())
 # Call the test_tv function to produce the output
 test_tv()
+
+# End of program
