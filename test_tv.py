2245252dd92fc94b6977e7c74a1e3df743f950bb
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-05-24 17:24:26 +0800

    Create instances of the class

--- test_tv.py
+++ test_tv.py
@@ -2,7 +2,10 @@
 
 # Import tv
 from tv import tv
+def test_tv():
 # Create instances of the class
+    tv_1 = tv()
+    tv_2 - tv()
 # Turn on the tv 1 
 # Set channel for tv 1
 # Set volume for tv 1
