7badd0d52ddf2259358acfab8a14a18d88530c8d
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-05-24 17:13:05 +0800

    Pseudocode

---
+++ test_tv.py
@@ -0,0 +1,11 @@
+# This program is the test driver that will produce the output from the class tv
+
+# Import tv
+# Create instances of the class
+# Turn on the tv 1 
+# Set channel for tv 1
+# Set volume for tv 1
+# Set channel for tv 2
+# Set volume for tv 2
+# Print the output
+# Call the test_tv function to produce the output
---
+++ tv.py
@@ -0,0 +1,16 @@
+# This program creates a class named tv and a test driver named test_tv that will create two objects from class tv
+
+# Define the tv class
+# Use constructor
+# Set the initial state of the tv
+# Turns on the tv
+# Turns off the tv
+# Get if the tv is on or off
+# Returns the channel for this tv
+# Set a new channel for the tv
+# Set a new volume for the tv
+# Increases the volume of the tv by 1
+# Decreases the volume of the tv by 1
+# Gets the volume level for this tv
+# Increases the channel number by 1
+# Decreases the channel number by 1
